648afcbb00f9e8cf2f87fff1085e57f19bea781f
Daniela <damilisa290@gmail.com>
2025-02-20 18:39:49 +0300

    init

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '@/views/HomePage.vue'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL + (process.env.NODE_ENV === 'development' ? '' : "/test-15-02")),
+    history: createWebHistory(process.env.NODE_ENV === 'development' ? '/' : "/test-15-02"),
     routes: [
         {
             path: '/',
